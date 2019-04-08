bddf40cde955e9fafee76d3f2aad506399f171f9
Esmee <esmeebarten@hotmail.com>
2019-04-09 00:15:55 +0200

    enter

--- opdracht3/java.js
+++ opdracht3/java.js
@@ -17,7 +17,11 @@ function ZoekFilms() {
     };
 }
 
-//ELEMENTEN AANMAKEN IN HTML
+//VIA ENTER BESTUREN
+function enterToets(event, data){
+    if (event.key === "Enter"){
+        console.log('enter');
+
 function createElement(data) {
     for (var item of data) {
         console.log(item['actors']);
@@ -25,6 +29,7 @@ function createElement(data) {
         //STANDAARD INFORMATIE
         var myArticle = document.createElement('article');
         myArticle.classList.add('content');
+
         var myH2 = document.createElement('h2');
         var myDate = document.createElement('h3');
         var myGerne = document.createElement('h4');
@@ -52,6 +57,11 @@ function createElement(data) {
         var leesMeerContainer = document.createElement('div');
         leesMeerContainer.classList.add('leesMeerContainer');
 
+        //AFBEELDING
+        var myIMG2 = document.createElement('img');
+        myIMG2.setAttribute('src', item.cover);
+        leesMeerContainer.appendChild(myIMG2);
+
         //LANGE PLOT
         var myPlot = document.createElement('p');
         myPlot.textContent = item.plot;
@@ -61,7 +71,7 @@ function createElement(data) {
         var myDirectorsTitel = document.createElement('h5');
         var myDirector = document.createElement('p');
 
-        myDirectorsTitel.textContent = "Director:"
+        myDirectorsTitel.textContent = "Director:";
         myDirector.textContent = item.directors[0].name;
 
         leesMeerContainer.appendChild(myDirectorsTitel);
@@ -75,7 +85,7 @@ function createElement(data) {
         var myActeurs3 = document.createElement('li');
         var myActeurs4 = document.createElement('li');
 
-        myBelangrijskteActeurs.textContent = "De belangrijkste acteurs:"
+        myBelangrijskteActeurs.textContent = "De belangrijkste acteurs:";
         myActeurs1.textContent = item.actors[0].actor_name + " speelt in de film " + item.actors[0].character ;
         myActeurs2.textContent = item.actors[1].actor_name + " speelt in de film " + item.actors[1].character ;
         myActeurs3.textContent = item.actors[2].actor_name + " speelt in de film " + item.actors[2].character ;
@@ -89,32 +99,18 @@ function createElement(data) {
         myActeursLijst.appendChild(myActeurs3);
         myActeursLijst.appendChild(myActeurs4);
 
-        //REVIEWS
-        var myReviews = document.createElement('h5');
-        var myReview1 = document.createElement('p');
-
-        myReviews.textContent = "Reviews";
-        myReview1.textContent = item.reviews;
-
-        leesMeerContainer.appendChild(myReviews);
-        leesMeerContainer.appendChild(myReview1);
-
-
-
         //LEESMEERCONTAINER IN ARTICLE PLAATSEN
         myArticle.appendChild(leesMeerContainer);
 
         //ARTICLE IN SECTION PLAATSEN
         document.querySelector('section').appendChild(myArticle);
-    }
-    event();
-}
-
-function event() {
+    }}
+    LeesMeerButton();
+}}
+function LeesMeerButton() {
     //LEES MEER BUTTONS SELECTEREN
     var buttons = document.querySelectorAll('.leesMeer');
 
-    //
     for (var button of buttons) {
         button.addEventListener('click', function () {
             var parent = this.parentElement;
@@ -122,8 +118,110 @@ function event() {
 
             parent.classList.toggle('active');
         });
-    }
+        }}
 
+
+ //VIA CLICK BESTUREN
+function createElement(data) {
+    for (var item of data) {
+        console.log(item['actors']);
+
+        //STANDAARD INFORMATIE
+        var myArticle = document.createElement('article');
+        myArticle.classList.add('content');
+
+        var myH2 = document.createElement('h2');
+        var myDate = document.createElement('h3');
+        var myGerne = document.createElement('h4');
+        var myMiniPlot = document.createElement('p');
+        var myIMG = document.createElement('img');
+        var myButton = document.createElement('button');
+        myButton.classList.add('leesMeer');
+
+        myH2.textContent = item.title;
+        myDate.textContent = item.release_date;
+        myGerne.textContent = item.genres;
+        myMiniPlot.textContent = item.simple_plot;
+        myIMG.setAttribute('src', item.cover);
+        myButton.textContent = 'Lees meer';
+
+        myArticle.appendChild(myIMG);
+        myArticle.appendChild(myH2);
+        myArticle.appendChild(myDate);
+        myArticle.appendChild(myGerne);
+        myArticle.appendChild(myMiniPlot);
+        myArticle.appendChild(myButton);
+
+
+        //MEER INFORMATIE WEERGEVEN
+        var leesMeerContainer = document.createElement('div');
+        leesMeerContainer.classList.add('leesMeerContainer');
+
+        //AFBEELDING
+        var myIMG3 = document.createElement('img');
+        myIMG3.setAttribute('src', item.cover);
+        leesMeerContainer.appendChild(myIMG3);
+
+        //LANGE PLOT
+        var myPlot = document.createElement('p');
+        myPlot.textContent = item.plot;
+        leesMeerContainer.appendChild(myPlot);
+
+        //DIRECTOR
+        var myDirectorsTitel = document.createElement('h5');
+        var myDirector = document.createElement('p');
+
+        myDirectorsTitel.textContent = "Director:";
+        myDirector.textContent = item.directors[0].name;
+
+        leesMeerContainer.appendChild(myDirectorsTitel);
+        leesMeerContainer.appendChild(myDirector);
+
+        //ACTEURS
+        var myBelangrijskteActeurs = document.createElement('h5');
+        var myActeursLijst = document.createElement ('ul');
+        var myActeurs1 = document.createElement('li');
+        var myActeurs2 = document.createElement('li');
+        var myActeurs3 = document.createElement('li');
+        var myActeurs4 = document.createElement('li');
+
+        myBelangrijskteActeurs.textContent = "De belangrijkste acteurs:";
+        myActeurs1.textContent = item.actors[0].actor_name + " speelt in de film " + item.actors[0].character ;
+        myActeurs2.textContent = item.actors[1].actor_name + " speelt in de film " + item.actors[1].character ;
+        myActeurs3.textContent = item.actors[2].actor_name + " speelt in de film " + item.actors[2].character ;
+        myActeurs4.textContent = item.actors[3].actor_name + " speelt in de film " + item.actors[3].character ;
+
+
+        leesMeerContainer.appendChild(myBelangrijskteActeurs);
+        leesMeerContainer.appendChild(myActeursLijst);
+        myActeursLijst.appendChild(myActeurs1);
+        myActeursLijst.appendChild(myActeurs2);
+        myActeursLijst.appendChild(myActeurs3);
+        myActeursLijst.appendChild(myActeurs4);
+
+        //LEESMEERCONTAINER IN ARTICLE PLAATSEN
+        myArticle.appendChild(leesMeerContainer);
+
+        //ARTICLE IN SECTION PLAATSEN
+        document.querySelector('section').appendChild(myArticle);
+    }
+    LeesMeerButton();
 }
 
+function LeesMeerButton() {
+    //LEES MEER BUTTONS SELECTEREN
+    var buttons = document.querySelectorAll('.leesMeer');
+
+    for (var button of buttons) {
+        button.addEventListener('click', function () {
+            var parent = this.parentElement;
+            console.log('click', parent);
+
+            parent.classList.toggle('active');
+        });
+    }}
+
+
+
 ZoekButton.addEventListener('click', ZoekFilms);
+window.addEventListener('keydown', enterToets);
